9b5dc76765da867ec4fa28ec9cb10ede1b1a1d3f
hadrich-hatem <hatem-hadrich.sifast@prestashop.com>
2018-07-23 14:38:20 +0100

    Add script of PR 9345

--- README.md
+++ README.md
@@ -24,13 +24,14 @@ https://github.com/NimaSoroush/differencify/blob/master/API.md
 #### Install test
 If you want to run the Install test you can run the script **installPrestashop**
 ```
-node test/not_mocha/installPrestashop.js --URL UrlOfShop --LANG language - --COUNTRY country -DB-DB_SERVER dataBaseServer --DB_USER dataBaseUsername --DB_PASSWD dataBasePassword
+node test/not_mocha/installPrestashop.js --URL UrlOfShop --LANG language - --COUNTRY country -DB-DB_SERVER dataBaseServer --DB_USER dataBaseUsername --DB_PASSWD dataBasePassword --RCTARGET lastStableVersionLocation
 
 ```
 
-* **URL**: **(Required)** Front office URL of your PrestaShop website without the “http://”
+* **URL**: **(Required)** Front office URL of your PrestaShop website with the “http://”
 * **LANG**: **(Optional)** Language to install with (default to "en")
 * **COUNTRY**: **(Optional)** Country to install with (default o "france")
 * **DB_SERVER**: **(Optional)** DataBase server (default to "mysql")
 * **DB_USER**: **(Optional)** DataBase user (default to "root")
 * **DB_PASSWD**: **(Optional)** DataBase password (default to "doge")
+* **RCTARGET**: **(Required)** Last stable version location directory (example: /project/prestashop1724/)
--- test/not_mocha/installPrestashop.js
+++ test/not_mocha/installPrestashop.js
@@ -1,129 +1,119 @@
 const puppeteer = require('puppeteer');
-let argv = require('minimist')(process.argv.slice(2));
+const globals = require('../globals');
 const chai = require('chai');
 chai.use(require('chai-string'));
 const expect = chai.expect;
+let fs = require('fs');
 
-const downloadLatestVersion = async () => {
-  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  await page.tracing.start({
-    path: 'downloadversion.json',
-    categories: ['devtools.timeline']
-  });
-  const client = await page.target().createCDPSession();
-  await client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './test/datas/'});
-  await page.goto('https://' + argv.UrlLastStableVersion);
-  await page.waitFor('body');
-  await page.click('#edit-submitted-termsconditions > div > label');
-  await page.click('#webform-client-form-501 > div > div.form-actions > input', {waitUntil: 'networkidle2'});
-
-  await page.waitFor(100000);
-  await page.tracing.stop();
-  await browser.close();
-};
-
-const InstallShop = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', `--window-size=${1500},${2000}`]
-  });
+const installShop = async (debugMode = false) => {
+  const browser = await puppeteer.launch({ headless: false, args: [`--window-size=${1500},${2000}`] });
   const page = await browser.newPage();
   await page.tracing.start({
     path: 'traceinstallshop.json',
     categories: ['devtools.timeline']
   });
 
-  await page.goto('http://' + argv.URL + 'install/');
+  if (debugMode) {
+    const definesFile = global.rcTarget + '/config/defines.inc.php';
+    await fs.readFile(definesFile, 'utf8', (err, content) => {
+      global.ps_mode_dev = (content.substring(content.indexOf("define('_PS_MODE_DEV_', "), content.indexOf(");")).split(', ')[1]) === 'true' ? true : false;
+      if (!global.ps_mode_dev) {
+        const result = content.replace("define('_PS_MODE_DEV_', false);", "define('_PS_MODE_DEV_', true);");
+        console.log(result);
+        fs.writeFile(definesFile, result, 'utf8', function (err) {
+          if (err) return console.log(err);
+          console.log("The Debug mode is activated!");
+        });
+      }
+    });
+  }
+
+  await page.goto(global.URL + 'install-dev/');
   await page.setViewport({width: 1500, height: 2000});
   await page.waitFor('body');
 
-  console.log('Choose your language');
+  console.log('Step 1 : Choose your language');
   // use page.select
+  await page.waitFor(2000);
   await page.waitFor('#langList');
-  await page.select('#langList', argv.LANG);
+  await page.select('#langList', global.language);
   await page.waitFor(2000);
   await page.waitFor('#btNext');
   await page.click('#btNext');
 
-  console.log('License agreements');
+  console.log('Step 2 : License agreements');
   // Accept the terms and conditions
   await page.waitFor('#set_license');
   await page.click('#set_license');
   await page.click('#btNext');
 
-  console.log('System compatibility');
+  console.log('Step 3 : System compatibility');
   // Checking prestashop compatibility
   await page.waitFor(2000);
-  await page.evaluate(() => document.querySelector('#sheet_ > h3').textContent).then((text) => {
+  await page.$eval('#sheet_ > h3', el => el.innerText).then((text) => {
     expect(text).to.be.equal('PrestaShop compatibility with your system environment has been verified!');
   });
   await page.click('#btNext');
 
-  console.log('Store information');
+  console.log('Step 4 : Store information');
   // Fill the information form
   await page.waitFor('#infosShop');
   await page.type('#infosShop', 'puppeteerDemo');
 
   await page.click('#infosCountry_chosen');
-  await page.type('#infosCountry_chosen > div > div > input[type="text"]', argv.COUNTRY);
+  await page.type('#infosCountry_chosen > div > div > input[type="text"]', global.country);
   await page.click('#infosCountry_chosen');
 
-  await page.type('#infosFirstname', 'Demo');
-  await page.type('#infosName', 'Prestashop');
-  await page.type('#infosEmail', 'demo@prestashop.com');
-  await page.type('#infosPassword', 'prestashop_demo');
-  await page.type('#infosPasswordRepeat', 'prestashop_demo');
+  await page.type('#infosFirstname', global.firstName);
+  await page.type('#infosName', global.lastName);
+  await page.type('#infosEmail', global.email);
+  await page.type('#infosPassword', global.password);
+  await page.type('#infosPasswordRepeat', global.password);
   await page.click('#btNext');
 
-  console.log('System configuration');
+  console.log('Step 5 : System configuration');
   // Fill the database configuration form
   await page.waitFor(3000);
-  await page.evaluate(() => { document.querySelector('input#dbServer').value = '' });
-  await page.type('#dbServer', argv.DB_SERVER);
-  await page.evaluate(() => { document.querySelector('input#dbName').value = '' });
-  await page.type('#dbName', 'database' + new Date().getTime());
-  await page.evaluate(() => { document.querySelector('input#dbLogin').value = '' });
-  await page.type('#dbLogin', argv.DB_USER);
-  if (argv.DB_PASSWD === 'undefined') {
-    await page.evaluate(() => { document.querySelector('input#dbPassword').value = '' });
+  await page.$eval('input#dbServer', (el, value) => el.value = value, global.dbServer);
+  await page.$eval('input#dbName', (el, value) => el.value = value, 'database' + global.dataTime);
+  await page.$eval('input#dbLogin', (el, value) => el.value = value, global.dbUser);
+  if (global.dbPassword === 'undefined') {
+    await page.$eval('input#dbPassword', (el, value) => el.value = value, '');
   } else {
-    await page.evaluate(() => { document.querySelector('input#dbPassword').value = '' });
-    await page.type('#dbPassword', argv.DB_PASSWD);
+    await page.$eval('input#dbPassword', (el, value) => el.value = value, global.dbPassword);
   }
   await page.click('#btTestDB');
   await page.waitFor(3000);
   await page.click('#btCreateDB');
   // Checking the database creation
   await page.waitFor(3000);
-  await page.evaluate(() => document.querySelector('#dbResultCheck').textContent).then((text) => {
-    console.log(text);
+  await page.$eval('#dbResultCheck', el => el.innerText).then((text) => {
+    console.log('\t ' + text);
     expect(text).to.be.equal('Database is created');
   });
   await page.waitFor(3000);
   await page.click('#btNext');
-  console.log('Store installation');
-  console.log('The installation is started');
+  console.log('Step 6 : Store installation');
+  console.log('\t The installation is started');
   // Waiting for installation
-  await page.waitForXPath('//li[@id="process_step_generateSettingsFile" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create file parameter'));
-  await page.waitForXPath('//li[@id="process_step_installDatabase" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create database'));
-  await page.waitForXPath('//li[@id="process_step_installDefaultData" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create default shop'));
-  await page.waitForXPath('//li[@id="process_step_populateDatabase" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create database table'));
-  await page.waitForXPath('//li[@id="process_step_configureShop" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create shop information'));
-  await page.waitForXPath('//li[@id="process_step_installFixtures" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create demonstration data'));
-  await page.waitForXPath('//li[@id="process_step_installModules" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create install module'));
-  await page.waitForXPath('//li[@id="process_step_installModulesAddons" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create addons modules'));
-  await page.waitForXPath('//li[@id="process_step_installTheme" and @class="process_step success"]', {timeout: 360000}).then(() => console.log('Create install theme'));
-  await page.waitForXPath('//*[@id="install_process_success"]/div[1]/h2').then(() => console.log('Finish installation'));
+  await page.waitForSelector('li[id=process_step_generateSettingsFile][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create file parameter'));
+  await page.waitForSelector('li[id=process_step_installDatabase][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create database'));
+  await page.waitForSelector('li[id=process_step_installDefaultData][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create default shop'));
+  await page.waitForSelector('li[id=process_step_populateDatabase][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create database table'));
+  await page.waitForSelector('li[id=process_step_configureShop][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create shop information'));
+  await page.waitForSelector('li[id=process_step_installFixtures][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create demonstration data'));
+  await page.waitForSelector('li[id=process_step_installModules][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create install module'));
+  await page.waitForSelector('li[id=process_step_installModulesAddons][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create addons modules'));
+  await page.waitForSelector('li[id=process_step_installTheme][class*="success"]', {timeout: 360000}).then(() => console.log('\t Create install theme'));
+  await page.waitForSelector('#install_process_success > div.clearfix > h2').then(() => console.log('Finish installation'));
 
   //Checking that the installation of shop is well finished
-  await page.evaluate(() => document.querySelector('#install_process_success > div.clearfix > h2').textContent).then((text) => {
+  await page.$eval('#install_process_success > div.clearfix > h2', el => el.innerText).then((text) => {
+    console.log('\t ' + text);
     expect(text).to.be.equal('Your installation is finished!');
   });
-
   await page.tracing.stop();
   await browser.close();
 };
 
-//downloadLatestVersion();
-InstallShop();
+installShop();
