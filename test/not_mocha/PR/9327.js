df28aae0d55527f6ebb1b4096c7f92f34d1a87d5
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2018-08-07 09:03:36 +0100

    TE: Check products when filtering by categories in the FO

--- test/not_mocha/PR/9327.js
+++ test/not_mocha/PR/9327.js
@@ -1,20 +1,20 @@
 const puppeteer = require('puppeteer');
-let argv = require('minimist')(process.argv.slice(2));
+require('../../globals');
 
 const checkProductPage = async () => {
-	const browser = await puppeteer.launch({ headless: false });
+	const browser = await puppeteer.launch({ headless: false, args: [`--window-size=${1280},${1024}`]});
 	const page = await browser.newPage();
 
 	await page.tracing.start({
-		path: 'trace.json',
+		path: '9327.json',
 		categories: ['devtools.timeline']
 	});
-	await page.goto(argv.URL);
+	await page.goto(global.URL + '/admin-dev');
 
-	await page.setViewport({ width: 1600, height: 2500 });
+	await page.setViewport({ width: 0, height: 0 });
 	await page.waitFor('body');
-	await page.type('#email', argv.LOGIN || 'demo@prestashop.com');
-	await page.type('#passwd', argv.PASSWD ||'prestashop_demo');
+	await page.type('#email', global.email);
+	await page.type('#passwd', global.password);
 	await page.click('#submit_login');
 	await page.waitFor('header');
 	await page.click('#subtab-ShopParameters');
