f61175560149271757c6c839d89512e909dc2b9d
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2018-11-14 08:58:49 +0100

    TE:PR-11312

---
+++ test/mocha/selectors/BO/shopParameters/productSettingsPage.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ProductSettingsPage: {
+    ProductsGeneral: {
+      max_size_short_description_input: '#form_general_short_description_limit',
+      save_button: '#configuration_fieldset_products button',
+      alert_text: '#main-div  div.alert.alert-danger p'
+    }
+  }
+};
